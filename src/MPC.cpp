172df50eef7364645136b21be4bc547e0939eb34
Hsin-Cheng Chao <olala7846@gmail.com>
2017-06-21 21:16:12 +0800

    Implement FG_eval function
    not yet tested

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,20 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 25;
+double dt = 0.4;
+
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N;
+
+// Speed limit for cost function
+double ref_v = 40;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -33,6 +45,78 @@ class FG_eval {
     // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
+
+    //
+    // Setup cost (fg[0]) to optimize
+    //
+    fg[0] = 0;
+
+    // Cost for reference state
+    for (int i = 0; i < N; i++) {
+      fg[0] += CppAD::pow(vars[cte_start + i], 2);
+      fg[0] += CppAD::pow(vars[epsi_start + i], 2);
+      fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);
+    }
+    // Minimize actuator use
+    for (int i = 0; i < N - 1; i++) {
+      fg[0] += CppAD::pow(vars[delta_start + i], 2);
+      fg[0] += CppAD::pow(vars[a_start + i], 2);
+    }
+
+    // Minimize actuator change
+    for (int i = 0; i < N - 2; i++) {
+      fg[0] += CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i] , 2);
+      fg[0] += CppAD::pow(vars[a_start + i + 1] - vars[a_start + i] , 2);
+    }
+
+    //
+    // Setup Constraints
+    //
+
+    // Initial state contraints are the same as state0
+    fg[1 + x_start] = vars[x_start];
+    fg[1 + y_start] = vars[y_start];
+    fg[1 + psi_start] = vars[psi_start];
+    fg[1 + v_start] = vars[v_start];
+    fg[1 + cte_start] = vars[cte_start];
+    fg[1 + epsi_start] = vars[epsi_start];
+
+    // The rest of the constraints
+    for (int i = 0; i < N - 1; i++) {
+      AD<double> x0 = vars[x_start + i];
+      AD<double> y0 = vars[y_start + i];
+      AD<double> psi0 = vars[psi_start + i];
+      AD<double> v0 = vars[v_start + i];
+      AD<double> cte0 = vars[cte_start + i];
+      AD<double> epsi0 = vars[epsi_start + i];
+
+      AD<double> x1 = vars[x_start + i + 1];
+      AD<double> y1 = vars[y_start + i + 1];
+      AD<double> psi1 = vars[psi_start + i + 1];
+      AD<double> v1 = vars[v_start + i + 1];
+      AD<double> cte1 = vars[cte_start + i + 1];
+      AD<double> epsi1 = vars[epsi_start + i + 1];
+
+      AD<double> delta0 = vars[delta_start + i];
+      AD<double> a0 = vars[a_start + i];
+
+      fg[x_start + i + 2] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[y_start + i + 2] = y1 = (y1 + v0 * CppAD::sin(psi0) * dt);
+      fg[psi_start + i + 2] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
+      fg[v_start + i + 2] = v1 - (v0 + a0 * dt);
+
+      // TODO(Olala): How many dimension are the coeffs?
+      AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0;
+      fg[cte_start + i + 2] =
+          cte1 - ((f0 - y0) + v0 * CppAD::sin(epsi0) * dt);
+
+      // TODO(Olala): how much is the tengent?
+      AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2]);
+      fg[epsi_start + i + 2] =
+          epsi1 - ((epsi0 - psides0) + v0 * delta0 / Lf * dt);
+
+    }
+
   }
 };
 
@@ -47,21 +131,27 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
 
-  // TODO: Set the number of model variables (includes both states and inputs).
-  // For example: If the state is a 4 element vector, the actuators is a 2
-  // element vector and there are 10 timesteps. The number of variables is:
-  //
-  // 4 * 10 + 2 * 9
-  size_t n_vars = 0;
+  double x = state[0];
+  double y = state[1];
+  double psi = state[2];
+  double v = state[3];
+  double cte = state[4];
+  double epsi = state[5];
+
+  // Set the number of model variables (includes both states and inputs).
+  // (x, y, psi, v, cte, e_psi), (delta, a)
+  size_t n_vars = N * 6 + (N - 1) * 2;
   // TODO: Set the number of constraints
-  size_t n_constraints = 0;
+  size_t n_constraints = N * 6;
 
   // Initial value of the independent variables.
   // SHOULD BE 0 besides initial state.
   Dvector vars(n_vars);
   for (int i = 0; i < n_vars; i++) {
-    vars[i] = 0;
+    vars[i] = 0.0;
   }
+  // Initial values
+  vars[x_start] = state[0];
 
   Dvector vars_lowerbound(n_vars);
   Dvector vars_upperbound(n_vars);
